7d8f76b02560fa5f642914402268c46469e1f153
Michael Krasnyk <michael.krasnyk@gmail.com>
2017-10-11 00:29:39 +0200

    Finish RouteService

--- README.md
+++ README.md
@@ -1,32 +1,57 @@
 # Open source routing machine ROS service
 
-http://project-osrm.org/
+ROS service for [OSRM](http://project-osrm.org/)
 
 ## How to build and run the service
 
 ### Build and setup local OSRM pipeline
+
 ```
-git clone https://github.com/Project-OSRM/osrm-backend/ -b 5.12 && osrm-backend
+sudo apt install libbz2-dev zlib1g-dev libboost-dev libexpat1-dev liblua5.2-dev libtbb-dev
+git clone https://github.com/Project-OSRM/osrm-backend/ -b 5.12 && cd osrm-backend
 mkdir build && cd build
 cmake .. -DBUILD_SHARED_LIBS=On && make -j4
 sudo make install
 ```
 
 ### Prepare routing data
+
+#### Download OSM data
+
 ```
 mkdir -p /tmp/data
 wget -P /tmp/data http://download.geofabrik.de/europe/monaco-latest.osm.pbf
-osrm-extract -p /usr/local/share/osrm/profiles/car.lua /tmp/data/monaco-latest.osm.pbf && osrm-contract /tmp/data/monaco-latest.osrm && osrm-datastore /tmp/data/monaco-latest.osrm
+```
+
+#### Data preprocessing
+
+```
+mkdir -p /tmp/data
+wget -P /tmp/data http://download.geofabrik.de/europe/monaco-latest.osm.pbf
+osrm-extract -p /usr/local/share/osrm/profiles/car.lua /tmp/data/monaco-latest.osm.pbf
+osrm-partition /tmp/data/monaco-latest.osrm
+osrm-customize /tmp/data/monaco-latest.osrm
+osrm-contract /tmp/data/monaco-latest.osrm
+```
+
+#### Load OSRM data into shared memory
+
+```
+osrm-datastore /tmp/data/monaco-latest.osrm
 ```
 
 ### Build and run the ROSRM service
+
 ```
 catkin_make
 source devel/setup.sh
 rosrun rosrm rosrm_server
 ```
 
-### Request some routes
+### ROSRM requests
+
 ```
 rosservice call /rosrm/route "waypoints: [{position: {'x':7.432251,'y':43.745995}}, {position: {'x':7.43062,'y':43.742139}}, {position: {'x':7.421576, 'y':43.734608}}]"
+
+rosservice call /rosrm/route "{waypoints: [{position: {'x':7.437959,'y':43.749614}}, {position: {'x':7.411480, 'y':43.730639}}], number_of_alternatives: 1, steps: true}"
 ```
--- src/rosrm_server.cpp
+++ src/rosrm_server.cpp
@@ -35,6 +35,104 @@ auto convert_coordinates(osrm::json::Array &coordinates)
     return result;
 }
 
+auto convert_lane(osrm::json::Object &lane)
+{
+    rosrm::Lane result;
+
+    result.valid = lane.values["valid"].which() == 4;
+    auto &indications = lane.values["indications"].get<osrm::util::json::Array>().values;
+    for (auto &indication : indications)
+    {
+        result.indications.emplace_back(indication.get<osrm::util::json::String>().value);
+    }
+
+    return result;
+}
+
+auto convert_intersection(osrm::json::Object &intersection)
+{
+    rosrm::Intersection result;
+
+    result.in = intersection.values.count("in") ? intersection.values["in"].get<osrm::util::json::Number>().value : 0;
+    result.out = intersection.values.count("out") ? intersection.values["out"].get<osrm::util::json::Number>().value : 0;
+    result.location = convert_coordinate(intersection.values["location"].get<osrm::util::json::Array>());
+
+    auto &bearings = intersection.values["bearings"].get<osrm::util::json::Array>().values;
+    for (auto &bearing : bearings)
+    {
+        result.bearings.emplace_back(bearing.get<osrm::util::json::Number>().value);
+    }
+
+    auto &entries = intersection.values["entry"].get<osrm::util::json::Array>().values;
+    for (auto &entry : entries)
+    {
+        result.entry.emplace_back(entry.which() == 4);
+    }
+
+    auto lanes = intersection.values.find("lanes");
+    if (lanes != intersection.values.end())
+    {
+        for (auto &lane : lanes->second.get<osrm::util::json::Array>().values)
+        {
+            result.lanes.emplace_back(convert_lane(lane.get<osrm::util::json::Object>()));
+        }
+    }
+
+    return result;
+}
+
+auto convert_maneuver(osrm::json::Object &maneuver)
+{
+    rosrm::Maneuver result;
+    result.type = maneuver.values["type"].get<osrm::util::json::String>().value;
+    result.modifier = maneuver.values["modifier"].get<osrm::util::json::String>().value;
+    result.exit = maneuver.values.count("exit") ? maneuver.values["exit"].get<osrm::util::json::Number>().value : 0;
+    result.bearing_before = maneuver.values["bearing_before"].get<osrm::util::json::Number>().value;
+    result.bearing_after = maneuver.values["bearing_after"].get<osrm::util::json::Number>().value;
+    result.location = convert_coordinate(maneuver.values["location"].get<osrm::util::json::Array>());
+
+    return result;
+}
+
+auto convert_step(osrm::json::Object &step)
+{
+    rosrm::Step result;
+    result.name = step.values["name"].get<osrm::util::json::String>().value;
+    result.mode = step.values["mode"].get<osrm::util::json::String>().value;
+    result.distance = step.values["distance"].get<osrm::util::json::Number>().value;
+    result.duration = step.values["duration"].get<osrm::util::json::Number>().value;
+    result.weight = step.values["weight"].get<osrm::util::json::Number>().value;
+    result.maneuver = convert_maneuver(step.values["maneuver"].get<osrm::util::json::Object>());
+
+    auto &geometry = step.values["geometry"].get<osrm::util::json::Object>().values;
+    result.coordinates = convert_coordinates(geometry["coordinates"].get<osrm::util::json::Array>());
+
+    auto &intersections = step.values["intersections"].get<osrm::util::json::Array>().values;
+    for (auto &intersection : intersections)
+    {
+        result.intersections.emplace_back(convert_intersection(intersection.get<osrm::util::json::Object>()));
+    }
+
+    return result;
+}
+
+auto convert_leg(osrm::json::Object &leg)
+{
+    rosrm::Leg result;
+    result.summary = leg.values["summary"].get<osrm::util::json::String>().value;
+    result.distance = leg.values["distance"].get<osrm::util::json::Number>().value;
+    result.duration = leg.values["duration"].get<osrm::util::json::Number>().value;
+    result.weight = leg.values["weight"].get<osrm::util::json::Number>().value;
+
+    auto &steps = leg.values["steps"].get<osrm::util::json::Array>().values;
+    for (auto &step : steps)
+    {
+        result.steps.emplace_back(convert_step(step.get<osrm::util::json::Object>()));
+    }
+
+    return result;
+}
+
 auto convert_route(osrm::json::Object &route)
 {
     rosrm::Route result;
@@ -45,6 +143,12 @@ auto convert_route(osrm::json::Object &route)
 
     auto &geometry = route.values["geometry"].get<osrm::util::json::Object>().values;
     result.coordinates = convert_coordinates(geometry["coordinates"].get<osrm::util::json::Array>());
+
+    auto &legs = route.values["legs"].get<osrm::util::json::Array>().values;
+    for (auto &leg : legs)
+    {
+        result.legs.emplace_back(convert_leg(leg.get<osrm::util::json::Object>()));
+    }
 
     return result;
 }
@@ -70,6 +174,10 @@ struct OSRMProxy
         // Set route parameters
         osrm::RouteParameters parameters;
 
+        // Set fixed parameters
+        parameters.geometries = osrm::engine::api::RouteParameters::GeometriesType::GeoJSON;
+
+        // Set request parameters
         for (const auto &waypoint : req.waypoints)
         {
             auto longitude = osrm::util::FloatLongitude{waypoint.position.x};
@@ -77,23 +185,27 @@ struct OSRMProxy
             parameters.coordinates.emplace_back(longitude, latitude);
         }
 
-        // Set fixed parameters
-        parameters.geometries = osrm::engine::api::RouteParameters::GeometriesType::GeoJSON;
+        for (const auto &radius : req.radiuses)
+        {
+            parameters.radiuses.push_back(radius == 0. ? boost::optional<double>() : radius);
+        }
 
-        // TODO: add parameters
+        for (const auto &bearing : req.bearings)
+        {
+            parameters.bearings.push_back(osrm::engine::Bearing{bearing.bearing, bearing.range});
+        }
 
-        // std::vector<boost::optional<Hint>> hints;
-        // std::vector<boost::optional<double>> radiuses;
-        // std::vector<boost::optional<Bearing>> bearings;
-        // std::vector<boost::optional<Approach>> approaches;
-        // std::vector<std::string> exclude;
+        for (const auto &approach : req.approaches)
+        {
+            parameters.approaches.push_back(static_cast<osrm::engine::Approach>(approach));
+        }
 
-        // bool steps = false;
-        // unsigned number_of_alternatives = 0;
-        // bool annotations = false;
-        // AnnotationsType annotations_type = AnnotationsType::None;
-        // OverviewType overview = OverviewType::Simplified;
-        // boost::optional<bool> continue_straight;
+        parameters.exclude = req.exclude;
+        parameters.steps = req.steps;
+        parameters.number_of_alternatives = req.number_of_alternatives;
+        parameters.annotations_type = static_cast<osrm::engine::api::RouteParameters::AnnotationsType>(req.annotation);
+        parameters.overview = static_cast<osrm::engine::api::RouteParameters::OverviewType>(req.overview);
+        parameters.continue_straight = req.continue_straight;
 
         // Find a route
         osrm::json::Object response;
@@ -178,7 +290,12 @@ struct OSRMProxy
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "rosrm_server");
-    ros::NodeHandle n;
+    ros::NodeHandle n("~");
+
+    std::vector< std::string > keys;
+    n.getParamNames(keys);
+    for (auto x : keys)
+        std::cout << x << "\n";
 
     // Get parameters and fill config structures
     osrm::EngineConfig config;
@@ -190,26 +307,38 @@ int main(int argc, char **argv)
     std::string algorithm;
     n.param<std::string>("algorithm", algorithm, "CH");
     std::transform(algorithm.begin(), algorithm.end(), algorithm.begin(), ::tolower);
-    config.algorithm = algorithm == "mld" ? osrm::engine::EngineConfig::Algorithm::MLD
-        : algorithm == "corech" ? osrm::engine::EngineConfig::Algorithm::CoreCH
-        : osrm::engine::EngineConfig::Algorithm::CH;
+    config.algorithm =
+        algorithm == "mld" ? osrm::engine::EngineConfig::Algorithm::MLD :
+        algorithm == "corech" ? osrm::engine::EngineConfig::Algorithm::CoreCH :
+        osrm::engine::EngineConfig::Algorithm::CH;
 
     n.param("use_shared_memory", config.use_shared_memory, base_path.empty());
 
-
     // Create OSRM engine and ROS<->OSRM proxy
-    osrm::OSRM osrm(config);
-    OSRMProxy proxy(osrm);
+    ROS_INFO("Starting ROSRM with %s using %s",
+             config.use_shared_memory ? "shared memory" : base_path.c_str(),
+             algorithm.c_str());
 
-    // Advertise OSRM service
-    ros::ServiceServer service = n.advertiseService("rosrm/route", &OSRMProxy::route, &proxy);
-    ros::ServiceServer service1 = n.advertiseService("rosrm/match", &OSRMProxy::match, &proxy);
+    try
+    {
+        osrm::OSRM osrm(config);
+        OSRMProxy proxy(osrm);
 
-    // Start service loop
-    ROS_INFO("ROSRM is ready");
-    ros::AsyncSpinner spinner(0);
-    spinner.start();
-    ros::waitForShutdown();
+        // Advertise OSRM service
+        ros::ServiceServer service = n.advertiseService("route", &OSRMProxy::route, &proxy);
+        ros::ServiceServer service1 = n.advertiseService("match", &OSRMProxy::match, &proxy);
+
+        // Start service loop
+        ROS_INFO("ROSRM is ready");
+        ros::AsyncSpinner spinner(0);
+        spinner.start();
+        ros::waitForShutdown();
+    }
+    catch(const std::exception& exc)
+    {
+        ROS_ERROR(exc.what());
+        return 1;
+    }
 
     return 0;
 }
